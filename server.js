ce051721c82a744a71cb1cd3c8ad4bab483d58dc
Sally Haefling <sallyhaefling@gmail.com>
2019-06-26 14:37:23 -0600

    Add get endpoints for bikes and countries and tes in postman

--- server.js
+++ server.js
@@ -1,54 +1,48 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-//require database
+
 const express = require('express');
-//not sure if this is working below
-// const data = require('data');
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.use(express.json())
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), () => console.log(`App is running 😃 on port ${app.get('port')}`));
 
-app.listen(3000, () => {
-  console.log(`App is running 😃 on port ${port}`);
-});
-
-//this below creates the new api path
-app.get('/api/v1/papers', (request, response) => {
-  database('papers').select()
-  .then((papers) => {
-    response.status(200).json(papers);
+app.get('/api/v1/bikes', (request, response) => {
+  database('bikes').select()
+  .then((bikes) => {
+    response.status(200).json(bikes);
   })
   .catch((error) => {
     response.status(500).json({ error });
   })
 });
 
-app.get('/api/v1/footnotes', (request, response) => {
-  database('footnotes').select()
-  .then((footnotes) => {
-    response.status(200).json(footnotes);
+app.get('/api/v1/countries', (request, response) => {
+  database('countries').select()
+  .then((countries) => {
+    response.status(200).json(countries);
   })
   .catch((error) => {
     response.status(500).json({ error });
   })
 });
 
-app.post('/api/v1/papers', (request, response) => {
-  const paper = request.body;
+app.post('/api/v1/bikes', (request, response) => {
+  const bike = request.body;
 
-  for (let requiredParameter of ['title', 'author']) {
-    if (!paper[requiredParameter]) {
+  for (let requiredParameter of ['country', 'name']) {
+    if (!bike[requiredParameter]) {
       return response
         .status(422)
-        .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+        .send({ error: `Expected format: { country: <String>, name: <String> }. You're missing a "${requiredParameter}" property.` });
     }
   }
 
-  database('papers').insert(paper, 'id')
-    .then(paper => {
-      response.status(201).json({ id: paper[0] })
+  database('bikes').insert(bike, 'id')
+    .then(bike => {
+      response.status(201).json({ id: bike[0] })
     })
     .catch(error => {
       response.status(500).json({ error });
