021b3a0aa9c28fd6774b1e1a5f206b9f2171004e
Sally Haefling <sallyhaefling@gmail.com>
2019-06-28 09:34:35 -0600

    Horoku deployment

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,14 @@ module.exports = {
     seeds: {
       directory: './db/seeds/dev'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
+  //postgres://shawomehknjmmq:89f5e03252db3b82b01ac9192497ae87ae9853f927354bcd9caaf9eb31be6ceb@ec2-107-22-211-248.compute-1.amazonaws.com:5432/d94cc7du1b82s9
 };
